f663eb7612a6751a084da15a48441449f35df576
Brandon Hsu <bphsu12@gmail.com>
2025-09-24 18:34:04 -0600

    Update Shape.java
    conflict merge

--- static2/app/src/main/java/com/ualberta/static2/Shape.java
+++ static2/app/src/main/java/com/ualberta/static2/Shape.java
@@ -3,5 +3,5 @@ package static2.app.src.main.java.com.ualberta.static2;
 public abstract class Shape {
     private Integer x;
     private Integer y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "$PUT_YOUR_CHOICE_OF_COLORs";
 }
