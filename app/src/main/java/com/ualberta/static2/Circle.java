ae7e63a8e66763839f87df3e257fed8f1a3de5c9
Mykyta Kuznietsov <kuznimyk@gmail.com>
2025-09-25 00:49:20 -0600

    deleted conflicts

--- app/src/main/java/com/ualberta/static2/Circle.java
+++
@@ -1,5 +0,0 @@
-package com.ualberta.static2;
-
-public class Circle extends Shape {
-    
-}
--- app/src/main/java/com/ualberta/static2/Shape.java
+++
@@ -1,7 +0,0 @@
-package com.ualberta.static2;
-
-public abstract class Shape {
-    private Integer x;
-    private Integer y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLORs";
-}
