64627d9ea0175ebc33784227af13c70a40a79932
Mykyta Kuznietsov <kuznimyk@gmail.com>
2025-09-25 00:49:20 -0600

    deleted conflicts

--- app/src/main/java/com/ualberta/static2/Shape2.java
+++
@@ -1,7 +0,0 @@
-package com.ualberta.static2;
-
-import static2.app.src.main.java.com.ualberta.static2.Shape;
-
-public class Shape2 extends Shape {
-
-}
--- app/src/main/java/com/ualberta/static2/Triangle.java
+++
@@ -1,7 +0,0 @@
-package static2.app.src.main.java.com.ualberta.static2;
-
-import java.awt.Shape;
-
-public class Triangle extends Shape {
-
-}
