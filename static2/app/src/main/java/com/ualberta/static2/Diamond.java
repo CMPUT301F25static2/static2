bb4c3684036aedd5dd2b86f31f5dad7f42d9f788
lumbani-z <lumbani@ualberta.ca>
2025-09-24 18:38:07 -0600

    .\static2\app\src\main\java\com\ualberta\static2\Diamond.java

--- static2/app/src/main/java/com/ualberta/static2/Diamond.java
+++ static2/app/src/main/java/com/ualberta/static2/Diamond.java
@@ -1,5 +1,5 @@
 package com.ualberta.static2;
 
-public class Diamond extends Shape {
-        
+public class Diamond extends static2.app.src.main.java.com.ualberta.static2.Shape {
+    String color = "blue";
 }
