16d2b33741f789975220623395678f678e618f7d
pbangit <bangit@ualberta.ca>
2025-10-26 16:02:01 -0600

    Started skeleton for EventLotteryProject

---
+++ EventLotteryProject/app/src/main/java/com/example/eventlotteryproject/EventFragments.java
@@ -0,0 +1,25 @@
+package com.example.eventlotteryproject;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+public class EventFragments extends Fragment {
+    private Button btnMyEvents, btnAvailableEvents;
+    private TextView titleText;
+    private EditText searchInput;
+
+
+}
