9e5d473c2a02147085a548b2550670bf48bb27a1
pbangit <bangit@ualberta.ca>
2025-10-27 20:38:26 -0600

    Added home screen functionality

--- EventLotteryProject/app/src/main/java/com/example/eventlotteryproject/Event.java
+++ EventLotteryProject/app/src/main/java/com/example/eventlotteryproject/Event.java
@@ -4,11 +4,13 @@ public class Event {
     private String title;
     private String eventDetails;
     private String timeFrame;
+    private String eventStatus;
 
-    public Event(String title, String eventDetails, String timeFrame){
+    public Event(String title, String eventDetails, String timeFrame, String eventStatus){
         this.title = title;
         this.eventDetails = eventDetails;
         this.timeFrame = timeFrame;
+        this.eventStatus = eventStatus;
     }
 
     public String getTitle() {
@@ -33,5 +35,13 @@ public class Event {
 
     public void setTimeFrame(String timeFrame) {
         this.timeFrame = timeFrame;
+    }
+
+    public String getEventStatus() {
+        return eventStatus;
+    }
+
+    public void setEventStatus(String eventStatus) {
+        this.eventStatus = eventStatus;
     }
 }
