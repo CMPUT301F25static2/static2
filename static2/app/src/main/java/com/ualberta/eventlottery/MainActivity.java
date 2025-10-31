4cf764e900d07406508ca5628b6f15384edd0733
pbangit <bangit@ualberta.ca>
2025-10-31 15:26:10 -0600

    Launching of Entrant Main Activity from Main Activity

--- static2/app/src/main/java/com/ualberta/eventlottery/MainActivity.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/MainActivity.java
@@ -11,16 +11,13 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import com.ualberta.eventlottery.admin.AdminMainActivity;
+import com.ualberta.eventlottery.entrant.EntrantMainActivity;
 import com.ualberta.static2.R;
 import com.ualberta.static2.databinding.ActivityEventLotteryMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "EventLottery";
-
     private ActivityEventLotteryMainBinding binding;
 
-    private boolean isAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,28 +29,17 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_event_lottery_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        isAdmin = false; // Change to false to go to entrant screens
-        if (isAdmin){
-            Intent myIntent = new Intent(MainActivity.this, AdminMainActivity.class);
-            MainActivity.this.startActivity(myIntent);
-        }
+        Class activity = EntrantMainActivity.class;
 
-        // https://stackoverflow.com/questions/36079523/launch-app-or-play-store-by-scanning-qr-code
-        // https://www.google.com/search?q=deeplink+testing+android+emulator&oq=deeplink+testing+android+emulator&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yDQgCEAAYhgMYgAQYigUyDQgDEAAYhgMYgAQYigUyDQgEEAAYhgMYgAQYigUyBwgFEAAY7wXSAQg2ODU3ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8
-        // https://stackoverflow.com/questions/9867410/barcode-scanning-in-android-emulator
-        //
-        // Make sure the device in the emulator is configured to use your web cam to test the bar code scanning
-        //
-        String action = getIntent().getAction();
-        String eventId = null;
-        if(Intent.ACTION_VIEW.equals(action)){
-            Uri uri = getIntent().getData();
-            if(uri != null){
-                eventId = uri.getQueryParameter("eventId");
-            }
-        }
-        Log.d(TAG, "Launched with eventId=" + eventId);
+        String userRole = "entrant"; // Change to admin or organizer to go to the admin or organizer screens
+        if (userRole.compareTo("admin") == 0){
+            activity = AdminMainActivity.class;
 
+        } else{
+            activity = EntrantMainActivity.class;
+        }
+        Intent myIntent = new Intent(MainActivity.this, activity);
+        MainActivity.this.startActivity(myIntent);
     }
 
 }
