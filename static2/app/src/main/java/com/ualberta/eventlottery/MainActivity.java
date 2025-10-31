2f98200fd79e95bf3ce27a85fb3123558a10b900
pbangit <bangit@ualberta.ca>
2025-10-31 14:46:18 -0600

    Initial QR code support

--- static2/app/src/main/java/com/ualberta/eventlottery/MainActivity.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ualberta.eventlottery;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -13,6 +15,8 @@ import com.ualberta.static2.R;
 import com.ualberta.static2.databinding.ActivityEventLotteryMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "EventLottery";
 
     private ActivityEventLotteryMainBinding binding;
 
@@ -33,6 +37,22 @@ public class MainActivity extends AppCompatActivity {
             Intent myIntent = new Intent(MainActivity.this, AdminMainActivity.class);
             MainActivity.this.startActivity(myIntent);
         }
+
+        // https://stackoverflow.com/questions/36079523/launch-app-or-play-store-by-scanning-qr-code
+        // https://www.google.com/search?q=deeplink+testing+android+emulator&oq=deeplink+testing+android+emulator&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIICAEQABgWGB4yDQgCEAAYhgMYgAQYigUyDQgDEAAYhgMYgAQYigUyDQgEEAAYhgMYgAQYigUyBwgFEAAY7wXSAQg2ODU3ajBqN6gCALACAA&sourceid=chrome&ie=UTF-8
+        // https://stackoverflow.com/questions/9867410/barcode-scanning-in-android-emulator
+        //
+        // Make sure the device in the emulator is configured to use your web cam to test the bar code scanning
+        //
+        String action = getIntent().getAction();
+        String eventId = null;
+        if(Intent.ACTION_VIEW.equals(action)){
+            Uri uri = getIntent().getData();
+            if(uri != null){
+                eventId = uri.getQueryParameter("eventId");
+            }
+        }
+        Log.d(TAG, "Launched with eventId=" + eventId);
 
     }
 
