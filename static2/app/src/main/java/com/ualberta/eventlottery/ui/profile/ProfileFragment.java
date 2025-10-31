ac510ac01e49c64cb960f0c6e5ce878bead31b7f
ryonryanzhou <zhouryan198@gmail.com>
2025-10-31 16:26:09 -0600

    ProfileUpdate skeleton

--- static2/app/src/main/java/com/ualberta/eventlottery/ui/profile/ProfileFragment.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/ui/profile/ProfileFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,18 +14,41 @@ import com.ualberta.static2.databinding.FragmentProfileBinding;
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private ProfileViewModel profileViewModel;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        profileViewModel = new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        final TextView textView = binding.textProfile;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        // --- 1. Observe LiveData to update TextViews automatically ---
+        profileViewModel.getName().observe(getViewLifecycleOwner(), name ->
+                binding.textName.setText(name)
+        );
+
+        profileViewModel.getEmail().observe(getViewLifecycleOwner(), email ->
+                binding.textEmail.setText(email)
+        );
+
+
+
+        // --- 2. Populate EditTexts with current values ---
+        binding.editName.setText(profileViewModel.getName().getValue());
+        binding.editEmail.setText(profileViewModel.getEmail().getValue());
+
+
+        // --- 3. Save button updates the ViewModel ---
+        binding.buttonSave.setOnClickListener(v -> {
+            profileViewModel.setName(binding.editName.getText().toString());
+            profileViewModel.setEmail(binding.editEmail.getText().toString());
+        });
+
+        // TODO: Later load data from Firebase if needed
+
+
+        return binding.getRoot();
     }
 
     @Override
