1287181431be8aa75959601ce30215cc913c9c20
pbangit <bangit@ualberta.ca>
2025-10-31 15:26:10 -0600

    Launching of Entrant Main Activity from Main Activity

--- static2/app/src/main/java/com/ualberta/eventlottery/entrant/EntrantMainActivity.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/entrant/EntrantMainActivity.java
@@ -1,26 +1,48 @@
 package com.ualberta.eventlottery.entrant;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
 import com.ualberta.static2.R;
+import com.ualberta.static2.databinding.ActivityEntrantMainBinding;
 
 
 public class EntrantMainActivity extends AppCompatActivity {
+    private static final String TAG = "EventLottery";
+
+    private ActivityEntrantMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_entrant_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.entrant_main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        binding = ActivityEntrantMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_entrant_main);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Sources used for QR code scanning
+        // https://stackoverflow.com/questions/36079523/launch-app-or-play-store-by-scanning-qr-code
+        // https://stackoverflow.com/questions/9867410/barcode-scanning-in-android-emulator
+        //
+        // Make sure the device in the emulator is configured to use your web cam to test the bar code scanning
+        //
+        String action = getIntent().getAction();
+        String eventId = null;
+        if(Intent.ACTION_VIEW.equals(action)){
+            Uri uri = getIntent().getData();
+            if(uri != null){
+                eventId = uri.getQueryParameter("eventId");
+            }
+        }
+        Log.d(TAG, "Launched with eventId=" + eventId);
     }
 }
