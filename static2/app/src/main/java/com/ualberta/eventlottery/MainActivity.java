d96d8597ec7cf8db0691d20c57281ed072e2b6a5
yuheng <2513807550@qq.com>
2025-11-01 14:52:03 -0600

    add main layout, add event list adapter

--- static2/app/src/main/java/com/ualberta/eventlottery/MainActivity.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/MainActivity.java
@@ -2,6 +2,7 @@ package com.ualberta.eventlottery;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -9,6 +10,8 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
 import com.ualberta.eventlottery.admin.AdminMainActivity;
+import com.ualberta.eventlottery.entrant.EntrantMainActivity;
+import com.ualberta.eventlottery.organzier.OrganizerMainActivity;
 import com.ualberta.static2.R;
 import com.ualberta.static2.databinding.ActivityEventLotteryMainBinding;
 
@@ -18,21 +21,59 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isAdmin;
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//        binding = ActivityEventLotteryMainBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_event_lottery_main);
+//        NavigationUI.setupWithNavController(binding.navView, navController);
+//
+//        isAdmin = false; // Change to false to go to entrant screens
+//        if (isAdmin){
+//            Intent myIntent = new Intent(MainActivity.this, AdminMainActivity.class);
+//            MainActivity.this.startActivity(myIntent);
+//        }
+//
+//    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityEventLotteryMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setupClickListeners();
+    }
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_event_lottery_main);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+    private void setupClickListeners() {
 
-        isAdmin = false; // Change to false to go to entrant screens
-        if (isAdmin){
-            Intent myIntent = new Intent(MainActivity.this, AdminMainActivity.class);
-            MainActivity.this.startActivity(myIntent);
-        }
+        findViewById(R.id.btn_entrant).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EntrantMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        findViewById(R.id.btn_admin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AdminMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.btn_organizer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OrganizerMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
--- static2/app/src/main/java/com/ualberta/eventlottery/organzier/Dialog_custom_content.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/organzier/Dialog_custom_content.java
@@ -16,7 +16,7 @@ public class Dialog_custom_content extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.dialog_custom_content);
+        setContentView(R.layout.organizer_dialog_custom_content);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
