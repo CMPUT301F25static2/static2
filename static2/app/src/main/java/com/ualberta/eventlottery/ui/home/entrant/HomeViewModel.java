b7c9a7e90e257402f10cbf970a74b5f369af874f
pbangit <bangit@ualberta.ca>
2025-11-05 11:49:03 -0700

    Started to pull available events from database

--- static2/app/src/main/java/com/ualberta/eventlottery/ui/home/entrant/HomeViewModel.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/ui/home/entrant/HomeViewModel.java
@@ -1,19 +1,31 @@
 package com.ualberta.eventlottery.ui.home.entrant;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.ualberta.eventlottery.model.Event;
+
+import java.util.List;
 
 public class HomeViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final EventListLiveData eventListLiveData;
+
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference eventsRef = db.collection("events");
+        eventListLiveData = new EventListLiveData(eventsRef);
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    // Google search terms:
+    //  firebasefirestore iterate collection android java
+    //  android java recyclerview attached to firebase database
+    //  Firebase database android java design pattern example
+    //  firebasefirestore livedata recyclerview android java
+    public LiveData<List<Event>> getAvailableEvents() {
+        return eventListLiveData;
     }
 }
