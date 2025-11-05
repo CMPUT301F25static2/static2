fda7c756a90635405313a5d477f6d14b4576ac37
lumbani-z <lumbani@ualberta.ca>
2025-11-05 13:40:40 -0700

    Admin can click and open user profile

--- static2/app/src/main/java/com/ualberta/eventlottery/admin/AdminMainActivity.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/admin/AdminMainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import com.ualberta.eventlottery.ui.adminHome.AdminHomeFragment;
 import com.ualberta.static2.R;
@@ -20,6 +23,12 @@ public class AdminMainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
+        //binding = ActivityEventLotteryMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+
+        //NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_event_lottery_main);
+        //NavigationUI.setupWithNavController(binding.navView, navController);
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_main);
@@ -35,4 +44,5 @@ public class AdminMainActivity extends AppCompatActivity{
                     .commit();
         }
     }
+
 }
