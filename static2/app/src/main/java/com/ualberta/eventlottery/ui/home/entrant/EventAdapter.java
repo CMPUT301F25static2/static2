7674ff8f9624a2e28a5d2394555001a43e7acc47
pbangit <bangit@ualberta.ca>
2025-11-06 19:55:24 -0700

    Javadocs written for eventlottery.ui.home.entrant package

--- static2/app/src/main/java/com/ualberta/eventlottery/ui/home/entrant/EventAdapter.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/ui/home/entrant/EventAdapter.java
@@ -27,6 +27,11 @@ import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+/*
+ * Adapter class for displaying a list of events in a RecyclerView for entrants
+ * Handles the binding of event data to view holders, managers user interactions
+ * for registration and withdrawal, and displays real-time waitlist count
+ */
 public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
     private static SimpleDateFormat sdfWithoutYear = new SimpleDateFormat("MMM dd", Locale.CANADA);
     private static SimpleDateFormat sdfWithYear = new SimpleDateFormat("MMM dd, yyyy", Locale.CANADA);
@@ -39,14 +44,30 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
 
     private List<Event> eventList;
 
+    /**
+     * Constructs an EventAdapter with a specified eventList.
+     * @param eventList the list of events to display
+     */
     public EventAdapter(List<Event> eventList){
         this.eventList = eventList;
     }
 
+    /**
+     * Updates the adapter with a new event and notifies the observer of any changes.
+     * @param newEvents the list of new events to display
+     */
     public void updateEvents(List<Event> newEvents){
         this.eventList = newEvents;
         notifyDataSetChanged();
     }
+
+    /**
+     * Creates a new ViewHolder based on the event item layout.
+     * @param parent   The ViewGroup into which the new View will be added after it is bound to
+     *                 an adapter position.
+     * @param viewType The view type of the new View.
+     * @return
+     */
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -55,7 +76,16 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
         return new ViewHolder(view);
     }
 
-
+    /**
+     * Binds event data to a specified position in the ViewHolder
+     * Sets the event details including title, waitlist count, status, dates, and time.
+     * Configures action button based on user registration status and sets up the listeners
+     * for withdrawals and registrations.
+     *
+     * @param holder   The ViewHolder which should be updated to represent the contents of the
+     *                 item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
@@ -158,11 +188,18 @@ public class EventAdapter extends RecyclerView.Adapter<EventAdapter.ViewHolder>{
         return sessionStartTime.format(DateTimeFormatter.ofPattern("hh:mm a"));
     }
 
+    /**
+     * Returns the total number of events in the adapter
+     * @return the size of the event list
+     */
     @Override
     public int getItemCount(){
         return eventList.size();
     }
 
+    /**
+     * ViewHolder class for an event item in the containing Recycler View
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView eventTitle, entrantsNumber, eventFromTo, eventStatus, eventSessionStartTime, btnActionText;
         LinearLayout btnAction;
--- static2/app/src/main/java/com/ualberta/eventlottery/ui/home/entrant/HomeViewModel.java
+++ static2/app/src/main/java/com/ualberta/eventlottery/ui/home/entrant/HomeViewModel.java
@@ -9,15 +9,26 @@ import com.ualberta.eventlottery.repository.EventRepository;
 
 import java.util.List;
 
+/**
+ * ViewModel for the Home Fragment that manages event lists.
+ */
 public class HomeViewModel extends ViewModel {
 
     private final EventListLiveData availableEventListLiveData;
 
 
+    /**
+     * Constructs a HomeViewModel and initializes events from EventRepository
+     */
     public HomeViewModel() {
         availableEventListLiveData = EventRepository.getInstance().getAvailableEvents();
     }
 
+    /**
+     * Returns LiveData containing the list of available events.
+     * An available event is an event that is open for registration.
+     * @return LiveData object containing a list of available {@code Event} objects.
+     */
     public LiveData<List<Event>> getAvailableEvents() {
         return availableEventListLiveData;
     }
