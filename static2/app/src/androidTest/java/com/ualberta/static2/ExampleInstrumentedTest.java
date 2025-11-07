50d4aa30a9832f2db0867c34d87b0ca5ec0afd27
Mykyta Kuznietsov <kuznimyk@gmail.com>
2025-11-07 11:32:43 -0700

    us 01.01.01 white box testing
    # Conflicts:
    #	static2/.idea/androidTestResultsUserPreferences.xml

--- static2/app/src/androidTest/java/com/ualberta/static2/ExampleInstrumentedTest.java
+++ static2/app/src/androidTest/java/com/ualberta/static2/ExampleInstrumentedTest.java
@@ -23,6 +23,5 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.ualberta.static2", appContext.getPackageName());
     }
-
-
+    
 }
