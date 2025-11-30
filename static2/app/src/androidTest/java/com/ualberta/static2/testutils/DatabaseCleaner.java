87e99b60d607ddbe3ce1cddf52af7d2f2f94d614
pbangit <bangit@ualberta.ca>
2025-11-30 10:50:10 -0700

    fixed database cleaning of registrations

--- static2/app/src/androidTest/java/com/ualberta/static2/testutils/DatabaseCleaner.java
+++ static2/app/src/androidTest/java/com/ualberta/static2/testutils/DatabaseCleaner.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertTrue;
 import com.ualberta.eventlottery.model.Registration;
 import com.ualberta.eventlottery.repository.RegistrationRepository;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -16,40 +19,52 @@ public class DatabaseCleaner {
 
         RegistrationRepository registrationRepository = RegistrationRepository.getInstance();
 
-        RegistrationRepository.RegistrationListCallback callback = new RegistrationRepository.RegistrationListCallback() {
+        RegistrationListCallback registrationListCallback = new RegistrationListCallback(latch);
+        registrationRepository.getRegistrationsByEvent(eventId, registrationListCallback);
+        assertTrue("Timeout cleaning registrations for event " + eventId, latch.await(awaitMS, TimeUnit.MILLISECONDS));
+
+        List<Registration> registrations = registrationListCallback.getRegistrations();
+
+        CountDownLatch deleteRegistrationLatch = new CountDownLatch(registrations.size());
+        RegistrationRepository.BooleanCallback deleteRegistrationCallback = new RegistrationRepository.BooleanCallback() {
             @Override
-            public void onSuccess(List<Registration> registrations) {
-                CountDownLatch deleteRegistrationLatch = new CountDownLatch(registrations.size());
-                RegistrationRepository.BooleanCallback deleteRegistrationCallback = new RegistrationRepository.BooleanCallback() {
-                    @Override
-                    public void onSuccess(boolean result) {
-                        deleteRegistrationLatch.countDown();
-                    }
-
-                    @Override
-                    public void onFailure(Exception e) {
-                        // Let the await timeout
-                        assertTrue("Failed to delete registration: " + e.getMessage(), false);
-                    }
-                };
-                for (Registration registration : registrations) {
-                    registrationRepository.deleteRegistration(registration.getId(), deleteRegistrationCallback);
-                }
-                try {
-                    assertTrue("Timeout waiting for " + registrations.size() + " registrations to be deleted", deleteRegistrationLatch.await(10000, TimeUnit.MILLISECONDS));
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-
-                latch.countDown();
+            public void onSuccess(boolean result) {
+                deleteRegistrationLatch.countDown();
             }
 
             @Override
             public void onFailure(Exception e) {
                 // Let the await timeout
+                assertTrue("Failed to delete registration: " + e.getMessage(), false);
             }
         };
-        registrationRepository.getRegistrationsByEvent(eventId, callback);
-        assertTrue("Timeout cleaning registrations for event " + eventId, latch.await(awaitMS, TimeUnit.MILLISECONDS));
+        for (Registration registration : registrations) {
+            registrationRepository.deleteRegistration(registration.getId(), deleteRegistrationCallback);
+        }
+        assertTrue("Timeout waiting for " + registrations.size() + " registrations to be deleted", deleteRegistrationLatch.await(10000, TimeUnit.MILLISECONDS));
+    }
+
+    private static class RegistrationListCallback implements RegistrationRepository.RegistrationListCallback {
+        private List<Registration> registrations = new ArrayList<>();
+        private CountDownLatch latch;
+
+        public RegistrationListCallback(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        public List<Registration> getRegistrations() {
+            return registrations;
+        }
+
+        @Override
+        public void onSuccess(List<Registration> registrations) {
+            this.registrations = registrations;
+            this.latch.countDown();
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+
+        }
     }
 }
